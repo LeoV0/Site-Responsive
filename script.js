807bb88413429e93f380c064ea4aa8a2bc9f6367
leoV0 <leo.cigale@gmail.com>
2025-05-13 10:54:15 +0200

    Ajout de la gestion d'erreur

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 const myButton = document.querySelector("#submitButton");
 const emailInput = document.querySelector("#mailValue");
-
+const errorMessage = document.querySelector("#textError");
+const iconError = document.querySelector("#iconError");
 myButton.addEventListener("click", () => {
   emailValidation();
 });
 
+// function emailValidation() {
+//   const emailRegex = !/\S+@\S+.\S+/.test(email);
+//   if (emailInput.value.match(emailRegex)) {
+//     errorMessage.innerHTML = `The mail is sent with success`;
+//     errorMessage.style.color = "green";
+//     iconError.classList.remove("show");
+//     iconError.src = ""; // Clear src when hidden
+//     emailInput.style.border = "1px solid var(--desaturatedRed)";
+//     console.log("Valid email, icon hidden");
+//   } else {
+//     errorMessage.innerHTML = `Please provide a valid email`;
+//     errorMessage.style.color = "var(--softRed)";
+//     iconError.src = "images/icon-error.svg";
+//     iconError.classList.add("show");
+//     emailInput.style.border = "2px solid var(--softRed)";
+//     console.log("Invalid email, icon should be visible", {
+//       hasShowClass: iconError.classList.contains("show"),
+//       src: iconError.src,
+//       display: window.getComputedStyle(iconError).display,
+//     });
+//   }
+// }
+
 function emailValidation() {
-  if (emailInput.value.match(/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/)) {
-    // emailIcon.style.display = "inline";
-    // emailIcon.src = "check.svg";
-    // emailError.style.display = "none";
-    console.log("yes");
+  const email = emailInput.value.trim();
+
+  if (email === "") {
+    errorMessage.innerHTML = `Please provide a valid email`;
+    errorMessage.style.color = "var(--softRed)";
+    iconError.src = "images/icon-error.svg";
+    iconError.classList.add("show");
+    emailInput.style.border = "2px solid var(--softRed)";
+  } else if (!/\S+@\S+\.\S+/.test(email)) {
+    errorMessage.innerHTML = `Please provide a valid email`;
+    errorMessage.style.color = "var(--softRed)";
+    iconError.src = "images/icon-error.svg";
+    iconError.classList.add("show");
+    emailInput.style.border = "2px solid var(--softRed)";
   } else {
-    // emailIcon.style.display = "inline";
-    // emailIcon.src = "error.svg";
-    // emailError.style.display = "block";
-    console.log("no");
+    errorMessage.innerHTML = `The mail is sent with success`;
+    errorMessage.style.color = "green";
+    iconError.classList.remove("show");
+    iconError.src = ""; // Clear src when hidden
+    emailInput.style.border = "1px solid var(--desaturatedRed)";
+    // emailInput.value = "";
   }
 }
